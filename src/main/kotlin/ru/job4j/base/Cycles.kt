1ed7b2ec740a099d2d67556fa89cd45ac7a56c7d
DmK78 <dmk78@inbox.ru>
2019-10-22 12:06:59 +0500

    5. Циклы while и for

---
+++ src/main/kotlin/ru/job4j/base/Cycles.kt
@@ -0,0 +1,17 @@
+package ru.job4j.base
+
+fun draw(size: Int) {
+
+    for (value in 0 until size) {
+        val str = if (value < size / 2) "".padStart(value) + "X" + "X".padStart((size - value * 2) - 1)
+        else if (value > size / 2) "X".padStart(size - value) + "X".padStart((value - size / 2) * 2)
+        else "".padStart(
+            value
+        ) + "X"
+        println(str)
+    }
+}
+
+fun main() {
+    draw(11)
+}
