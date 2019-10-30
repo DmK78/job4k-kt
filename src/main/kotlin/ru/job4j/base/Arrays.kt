b303e7f8a99376ed1b77d2ea5ac8735694b292c9
DmK78 <dmk78@inbox.ru>
2019-10-30 19:12:35 +0500

    6. Массивы и коллекции

--- src/main/kotlin/ru/job4j/base/Arrays.kt
+++ src/main/kotlin/ru/job4j/base/Arrays.kt
@@ -1,23 +1,9 @@
 package ru.job4j.base
 
-
-fun main(args: Array<String>) {
-    val names = arrayOfNulls<String>(10)
-    names[0] = "Petr Arsentev"
-    names[3] = "Dmitry Kolganov"
-    val defragmentArray = defragment(names)
-    for (name in defragmentArray) {
-        println(name + " " + name?.length)
-    }
-
-
-}
-
-
-
-
 fun defragment(array: Array<String?>): Array<String?> {
     var result = arrayOfNulls<String>(array.size)
+
+
     var count = 0
     for (value in array) {
         if (!value.isNullOrBlank()) {
--- src/test/kotlin/ru/job4j/base/ArraysTest.kt
+++ src/test/kotlin/ru/job4j/base/ArraysTest.kt
@@ -5,22 +5,14 @@ import io.kotlintest.specs.StringSpec
 
 class ArraysTest : StringSpec({
     "defrag Array index3 should be index0" {
-        var names = arrayOfNulls<String>(5)
-        names[3] = "Ivan"
-        var expNames = arrayOfNulls<String>(5)
-        expNames[0]="Ivan"
+        var names = arrayOf(null, null, "Ivan", null, null)
+        var expNames = arrayOf("Ivan", null, null, null, null)
         defragment(names) shouldBe expNames
     }
 
     "defrag Array index234 wshould be index012" {
-        var names = arrayOfNulls<String>(5)
-        names[2] = "Ivan"
-        names[3] = "Ivan1"
-        names[4] = "Ivan2"
-        var expNames = arrayOfNulls<String>(5)
-        expNames[0]="Ivan"
-        expNames[1]="Ivan1"
-        expNames[2]="Ivan2"
+        var names = arrayOf(null, null, "Ivan", "Ivan1", "Ivan2")
+        var expNames = arrayOf("Ivan", "Ivan1", "Ivan2", null, null)
         defragment(names) shouldBe expNames
     }
 
