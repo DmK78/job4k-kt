5ae94c3ab48e3277c0f79f151bb18805cdff76a5
DmK78 <dmk78@inbox.ru>
2019-10-31 16:47:15 +0500

    1. Класс и объект[#191447]

--- src/main/kotlin/ru/job4j/tracker/Tracker.kt
+++ src/main/kotlin/ru/job4j/tracker/Tracker.kt
@@ -5,26 +5,22 @@ import java.util.ArrayList
 class Tracker {
     private val items = ArrayList<Item>()
 
-    fun add(item: Item): Item {
-        item.setId(generateId())
-        item.setTime(System.currentTimeMillis())
-        items.add(item)
-        return item
+    fun add(item: Item): Item = item?.let {
+        it.setId(generateId())
+        it.setTime(System.currentTimeMillis())
+        items.add(it)
+        return it
     }
 
     fun delete(id: Int): Boolean = items.remove(findById(id))
 
     fun getSize(): Int = items.size
 
-
-    fun replace(id: Int, item: Item): Boolean {
-        var findItem = findById(id)
-        if (findItem != null) {
-            findItem.setName(item.getName())
-            findItem.setDesc(item.getDesc())
-            return true
-        } else return false
-    }
+    fun replace(id: Int, item: Item): Boolean = findById(id)?.let {
+        it.setName(item.getName())
+        it.setDesc(item.getDesc())
+        return true
+    } ?: false
 
     fun findAll(): List<Item> = items
 
