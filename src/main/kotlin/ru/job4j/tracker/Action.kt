5bc380bb813956e9a9ad4bd8dc5e3d29d6682ef2
DmK78 <dmk78@inbox.ru>
2019-11-01 12:08:38 +0500

    7.1. Интерфейсы[#191455]

---
+++ src/main/kotlin/ru/job4j/tracker/Action.kt
@@ -0,0 +1,7 @@
+package ru.job4j.tracker
+
+interface Action {
+    fun add()
+    fun showAll()
+    fun exit()
+}
--- src/main/kotlin/ru/job4j/tracker/StartUi.kt
+++ src/main/kotlin/ru/job4j/tracker/StartUi.kt
@@ -1,28 +1,32 @@
 package ru.job4j.tracker
 
-import java.util.ArrayList
+class StartUi private constructor(): Action {
+    override fun add() {
+        println("Добавление заявки")
+    }
 
-class StartUi private constructor() {
+    override fun showAll() {
+        println("Печать всех заявок")
+    }
+
+    override fun exit() {
+        return
+    }
+
     companion object {
-        fun addItem(item: Item) {
-            println("Добавление заявки")
-        }
-
-        fun showAll() {
-            println("Печать всех заявок")
-        }
-
         fun init() {
             println("0 - Добавить заявку")
             println("1 - Показать все заявки")
             println("6 - Выйти")
+            val startUi = StartUi()
             do {
                 var i = readLine()!!.toInt()
                 when (i) {
-                    0 -> addItem(Item(name = "1", desc = "1"))
-                    1 -> showAll()
+                    0 -> startUi.add()
+                    1 -> startUi.showAll()
+                    6 -> return
                 }
-            } while (i != 6)
+            } while (true)
         }
     }
 }
