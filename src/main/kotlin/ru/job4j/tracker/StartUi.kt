3d1ccae2357c1553734ae84c31bb3845020f648c
DmK78 <dmk78@inbox.ru>
2019-11-01 19:43:37 +0500

    7.1. Интерфейсы[#191455]

--- src/main/kotlin/ru/job4j/tracker/StartUi.kt
+++ src/main/kotlin/ru/job4j/tracker/StartUi.kt
@@ -1,6 +1,20 @@
 package ru.job4j.tracker
 
-class StartUi private constructor(): Action {
+class StartUi : Action {
+    init {
+        println("0 - Добавить заявку")
+        println("1 - Показать все заявки")
+        println("6 - Выйти")
+        do {
+            var i = readLine()!!.toInt()
+            when (i) {
+                0 -> add()
+                1 -> showAll()
+                6 -> exit()
+            }
+        } while (true)
+    }
+
     override fun add() {
         println("Добавление заявки")
     }
@@ -10,27 +24,10 @@ class StartUi private constructor(): Action {
     }
 
     override fun exit() {
-        return
-    }
-
-    companion object {
-        fun init() {
-            println("0 - Добавить заявку")
-            println("1 - Показать все заявки")
-            println("6 - Выйти")
-            val startUi = StartUi()
-            do {
-                var i = readLine()!!.toInt()
-                when (i) {
-                    0 -> startUi.add()
-                    1 -> startUi.showAll()
-                    6 -> return
-                }
-            } while (true)
-        }
+        System.exit(0)
     }
 }
 
-fun main(args: Array<String>) {
-    StartUi.init()
+fun main() {
+    StartUi()
 }
