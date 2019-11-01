bfd8afff317a302b1add6058184358b276cd44e5
DmK78 <dmk78@inbox.ru>
2019-11-01 12:15:10 +0500

    8. Переопределение[#191456]

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -1,18 +1,41 @@
 package ru.job4j.oop
 
-open class Profession(val professionName: String, val professionDesc: String)
+open class Profession(val professionName: String, val professionDesc: String){
+    open fun action(){
+        println("do some action")
+    }
+}
 
 class Doctor(professionName: String, professionDesc: String, var specification: String = "") :
-    Profession(professionName, professionDesc)
+    Profession(professionName, professionDesc){
+    init {
+        println("doctor was created")
+    }
+    override fun action() {
+        //super.action()
+        println("doctor cure people")
+    }
+}
 
 class CarDriver(professionName: String, professionDesc: String, var license: String = "") :
-    Profession(professionName, professionDesc)
+    Profession(professionName, professionDesc){
+    init {
+        println("driver was created")
+    }
+    override fun action() {
+        //super.action()
+        println("driver drive the car")
+    }
+}
 
 class Worker(name: String, surName: String, var profession: Profession)
 
 fun main() {
     val doctor1 = Worker("Ivan", "Ivanov", Doctor("Doctor", "cure people", specification = "dentist"))
+    doctor1.profession.action()
     val driver1 = Worker("Semen", "Demchenko", CarDriver("Driver", "drive a car", license = "A"))
+    driver1.profession.action()
+
 
 
 }
